8b14fa52ee2ced6f9ebce4a7c6700716c7e6e436
Daniel Segesdi <segesdi.dani@gmail.com>
2014-05-31 22:44:18 +0200

    minor changes in danomano bot

--- vindinium/Bots/DanomanoBot.cs
+++ vindinium/Bots/DanomanoBot.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vindinium.SmartBoard;
 
 namespace Vindinium.Bots
 {
     public class DanomanoBot : Bot
     {
-
+        public DanomanoBot(ServerStuff serverStuff) : base(serverStuff)
+        {
+        }
 
         protected override void Play()
         {
@@ -16,45 +19,31 @@ namespace Vindinium.Bots
             while (GameState.IsFinished == false && ServerStuff.Errored == false)
             {
                 Position myPosition = GameState.MyHero.pos;
-                Position eastPosition = new Position(myPosition.x, myPosition.y+1);
-                Position northPosition = new Position(myPosition.x+1, myPosition.y);
-                Position westPosition = new Position(myPosition.x, myPosition.y-1);
-                Position southPosition = new Position(myPosition.x-1, myPosition.y);
-                Console.WriteLine("My pos: ({0}, {1})", myPosition.x, myPosition.y);
-                WriteGamestateToFile();
-                Console.WriteLine("East ({0}, {1})", eastPosition.x, eastPosition.y);
-                Console.WriteLine("North ({0}, {1})", northPosition.x, northPosition.y);
-                Console.WriteLine("West ({0}, {1})", westPosition.x, westPosition.y);
-                Console.WriteLine("South ({0}, {1})", southPosition.x, southPosition.y);
+                Tile eastTile = Board.GetTile(new Position(myPosition.x, myPosition.y+1));
+                Tile northTile = Board.GetTile(new Position(myPosition.x+1, myPosition.y));
+                Tile westTile = Board.GetTile(new Position(myPosition.x, myPosition.y-1));
+                Tile southTile = Board.GetTile(new Position(myPosition.x-1, myPosition.y));
 
-                if (Board.GetTile(eastPosition) != null)
+
+                if (eastTile != null)
                 {
-                    Console.WriteLine("Going east! Destination pos: ({0}, {1}), destination type: {2}",
-                        eastPosition.x, eastPosition.y, Board.GetTile(eastPosition).Type);
                     ServerStuff.MoveHero(Direction.East);
                 }
-                else if (Board.GetTile(northPosition) != null)
+                else if (northTile != null)
                 {
                     ServerStuff.MoveHero(Direction.North);
-                    Console.WriteLine("Going north! Destination pos: ({0}, {1}), destination type: {2}",
-                        northPosition.x, northPosition.y, Board.GetTile(northPosition).Type);
                 }
-                else if (Board.GetTile(westPosition) != null)
+                else if (westTile != null)
                 {
                     ServerStuff.MoveHero(Direction.West);
-                    Console.WriteLine("Going west! Destination pos: ({0}, {1}), destination type: {2}",
-                        westPosition.x, westPosition.y, Board.GetTile(westPosition).Type);
                 }
-                else if (Board.GetTile(southPosition) != null)
+                else if (southTile != null)
                 {
                     ServerStuff.MoveHero(Direction.South);
-                    Console.WriteLine("Going south! Destination pos: ({0}, {1}), destination type: {2}",
-                        southPosition.x, southPosition.y, Board.GetTile(southPosition).Type);
                 }
                 else
                 {
                     ServerStuff.MoveHero(Direction.Stay);
-                    Console.WriteLine("staying!");
                 }
 
                 Console.Out.WriteLine("completed turn " + ServerStuff.GameState.CurrentTurn);
@@ -68,7 +57,7 @@ namespace Vindinium.Bots
             Console.Out.WriteLine("danomano bot finished");
         }
 
-        private void WriteGamestateToFile()
+        private void WriteBoardToFile()
         {
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"c:\Users\dani\testfolder\vindinium.txt", true))
             {
@@ -82,11 +71,6 @@ namespace Vindinium.Bots
                     file.WriteLine();
                 }
             }
-        }
-
-        public DanomanoBot(ServerStuff serverStuff)
-            : base(serverStuff)
-        {
-        }
+        }      
     }
 }
