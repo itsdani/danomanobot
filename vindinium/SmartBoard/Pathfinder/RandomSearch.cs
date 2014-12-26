1bf929b37175fe9542c998539fb825708b63b136
Daniel Segesdi <segesdi.dani@gmail.com>
2014-12-26 23:45:06 +0100

    random search pathfinder implementation

---
+++ vindinium/SmartBoard/Pathfinder/RandomSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vindinium.SmartBoard.Pathfinder
+{
+    class RandomSearch : IPathFinder
+    {
+        private readonly Random _random = new Random();
+
+
+        public string GetDirection(Tile starTile, Tile destinationTile, Board board)
+        {
+            
+            switch(_random.Next(0, 5))
+            {
+                case 0:
+                    return Direction.East;
+                    break;
+                case 1:
+                    return Direction.North;
+                    break;
+                case 2:
+                    return Direction.South;
+                    break;
+                case 3:
+                    return Direction.West;
+                    break;
+                default:
+                    return Direction.Stay;
+                    break;
+            }
+        }
+    }
+}
